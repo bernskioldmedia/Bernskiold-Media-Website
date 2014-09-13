985cc8d6a277d86968fef0ab4ea583f0e1742c41
Erik Bernskiold <erik@bernskioldmedia.com>
2014-09-13 15:30:27 +0200

    GA code update
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- wp-content/themes/bernskioldmedia/core/functions/scripts-styles.php
+++ wp-content/themes/bernskioldmedia/core/functions/scripts-styles.php
@@ -110,7 +110,7 @@ function bernskioldmedia_google_analytics() {
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-19733539-4', 'auto');
+		ga('create', '<?php echo $property_id; ?>', 'auto');
 		ga('send', 'pageview');
 	</script>
 	<?php
