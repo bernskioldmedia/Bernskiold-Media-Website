d413dcf2249c1209f4807817b7cc04e81e142bfe
Erik Bernskiold <erik@bernskioldmedia.com>
2014-11-22 11:29:03 +0100

    Fix textdomain
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- wp-content/themes/bernskioldmedia/page-templates/template-service-page.php
+++ wp-content/themes/bernskioldmedia/page-templates/template-service-page.php
@@ -97,7 +97,7 @@ get_header(); ?>
 			<div class="row page-section">
 
 				<div class="small-24 medium-20 medium-centered columns text-center">
-					<h2><?php _e( 'Related Case Studies', '￼textdomain' ); ?></h2>
+					<h2><?php _e( 'Related Case Studies', 'bernskioldmedia' ); ?></h2>
 					<p class="intro"><?php _e( 'What better than reading about projects we have done previously for other clients as well as read what they thought about working with us and the results.', 'bernskioldmedia' ); ?></p>
 				</div>
 
