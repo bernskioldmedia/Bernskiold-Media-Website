a7a4007d91053d5b213c4773a1a303d3f053ca92
Erik Bernskiold <erik@bernskioldmedia.com>
2016-06-24 18:31:22 +0200

    Fix saving of ACF fields

--- content/plugins/bm-acf-fields/bm-acf-fields.php
+++ content/plugins/bm-acf-fields/bm-acf-fields.php
@@ -10,16 +10,37 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
-namespace BernskioldMedia\Website\ACF_Fields;
 
-//Change ACF Local JSON save location to /json folder inside this plugin
-add_filter( 'json/settings/save_json', function() {
-	return dirname( __FILE__ ) . '/json';
-} );
+// Hook into the filters.
+add_filter( 'acf/settings/save_json', 'bm_acf_json_save' );
+add_filter( 'acf/settings/load_json', 'bm_acf_json_load' );
 
-//Include the /json folder in the places to look for ACF Local JSON files
-add_filter( 'json/settings/load_json', function( $paths ) {
+/**
+ * Modify ACF JSON Save Folder
+ *
+ * @return string
+ */
+function bm_acf_json_save() {
+
+	// update path
+	$path = dirname( __FILE__ ) . '/json';
+
+	return $path;
+
+}
+
+/**
+ * Make ACF recognize custom JSON folder for loading
+ *
+ * @param $paths
+ *
+ * @return array
+ */
+function bm_acf_json_load( $paths ) {
+
+	// append path
 	$paths[] = dirname( __FILE__ ) . '/json';
 
 	return $paths;
-} );
+
+}
