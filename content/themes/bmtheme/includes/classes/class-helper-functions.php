85340ba2a6260b40c0ac4a151ea9ef0cf5f69b00
Erik Bernskiold <erik@bernskioldmedia.com>
2016-08-26 14:50:03 +0200

    Tweak clients archive

--- content/themes/bmtheme/includes/classes/class-helper-functions.php
+++ content/themes/bmtheme/includes/classes/class-helper-functions.php
@@ -22,6 +22,7 @@ class Theme_Helpers {
 	public function __construct() {
 
 		add_action( 'pre_get_posts', array( $this, 'modify_case_studies_query' ) );
+		add_action( 'pre_get_posts', array( $this, 'modify_clients_query' ) );
 
 	}
 
@@ -37,6 +38,35 @@ class Theme_Helpers {
 
 		$query->set( 'posts_per_page', 50 );
 		$query->set( 'meta_query', array(
+			array(
+				'key'     => 'is_item_active',
+				'value'   => '1',
+				'compare' => '=',
+			),
+		) );
+
+		return;
+
+	}
+
+	public function modify_clients_query( $query ) {
+
+		if ( is_admin() || ! $query->is_main_query() ) {
+			return;
+		}
+
+		if ( ! is_post_type_archive( 'bm_clients' ) ) {
+			return;
+		}
+
+		$query->set( 'posts_per_page', 100 );
+		$query->set( 'meta_query', array(
+			'relation' => 'AND',
+			array(
+				'key'     => 'client_logo_dark',
+				'value'   => '',
+				'compare' => '!=',
+			),
 			array(
 				'key'     => 'is_item_active',
 				'value'   => '1',
