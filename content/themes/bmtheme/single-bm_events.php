e066e0e6e512389d4fae32f47f4c45b0a4f2f7c1
Erik Bernskiold <erik@bernskioldmedia.com>
2017-01-16 22:15:40 +0100

    add VAT info

--- content/themes/bmtheme/single-bm_events.php
+++ content/themes/bmtheme/single-bm_events.php
@@ -130,7 +130,7 @@ get_header(); ?>
 									<?php if ( $_product ) {
 										echo $event_price;
 									} else {
-										printf( __( '%s USD', 'bmtheme' ), $event_price );
+										printf( __( '%s USD (excl. VAT)', 'bmtheme' ), $event_price );
 									} ?>
 								</dd>
 							<?php endif; ?>
